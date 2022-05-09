8e652afbd23c2f8a53eeb82daa3d5214dd82df20
corund1976 <corund1976@gmail.com>
2022-05-09 22:32:23 +0300

    getIP function

--- src/redux/user/helper.js
+++ src/redux/user/helper.js
@@ -6,7 +6,7 @@ export function get_cookie(cookie_name) {
   var results = document.cookie.match(
     '(^|;) ?' + cookie_name + '=([^;]*)(;|$)',
   );
-  console.log('redux/user/helper.js function "get_cookie(cookie_name)" => results=', results);
+  console.log('redux/user/helper.js function "get_cookie(cookie_name)" => results=', unescape(results[2]));
   if (results)
     return unescape(results[2]);
   else
@@ -15,41 +15,20 @@ export function get_cookie(cookie_name) {
 
 export async function getUserIP() {
   try {
-    console.log('redux/user/helper.js getUserIP');
+    console.log('redux/user/helper.js function "getUserIP"');
 
-    async function fetchCloudflare() {
-      const response = await fetch("https://www.cloudflare.com/cdn-cgi/trace", { mode: "cors" });
-      console.log(response);
+    const response = await fetch(
+      "https://www.cloudflare.com/cdn-cgi/trace",
+      { mode: "cors" }
+    );
+    const text = await response.text();
+    const textStringWithIP = text
+      .trim()
+      .split('\n')
+      .filter(item => item.includes('ip'))[0];
+    const ip = textStringWithIP.slice(textStringWithIP.indexOf('=') + 1);
 
-      const text = await response.text();
-      console.log(text);
-
-      // fl = 132f109
-      // h = www.cloudflare.com
-      // ip = 77.120.232.132
-      // ts = 1652123278.675
-      // visit_scheme = https
-      // uag = Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 101.0.4951.54 Safari / 537.36
-      // colo = KBP
-      // http = http / 2
-      // loc = UA
-      // tls = TLSv1.3
-      // sni = plaintext
-      // warp = off
-      // gateway = off
-
-      const ip = text
-        .trim()
-        .split('\n')
-        .filter(item => item.includes('ip'))[0];
-
-      console.log('ip', ip);
-
-      // const res = ip.slice(ip.indexOf('=') + 1);
-      // console.log('res', res);
-      return text;
-    }
-    fetchCloudflare()
+    return ip;
   } catch (error) {
     return false;
   }
