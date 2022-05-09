863691c2f65903ce7d2fc1e53befb9ab8003976f
corund1976 <corund1976@gmail.com>
2022-05-09 22:11:12 +0300

    fix

--- src/redux/user/helper.js
+++ src/redux/user/helper.js
@@ -24,12 +24,26 @@ export async function getUserIP() {
       const text = await response.text();
       console.log(text);
 
-      // const ip = response
-      //   .trim()
-      //   .split('\n')
-      //   .filter(item => item.includes('ip'))[0];
+      // fl = 132f109
+      // h = www.cloudflare.com
+      // ip = 77.120.232.132
+      // ts = 1652123278.675
+      // visit_scheme = https
+      // uag = Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 101.0.4951.54 Safari / 537.36
+      // colo = KBP
+      // http = http / 2
+      // loc = UA
+      // tls = TLSv1.3
+      // sni = plaintext
+      // warp = off
+      // gateway = off
 
-      // console.log('ip', ip);
+      const ip = text
+        .trim()
+        .split('\n')
+        .filter(item => item.includes('ip'))[0];
+
+      console.log('ip', ip);
 
       // const res = ip.slice(ip.indexOf('=') + 1);
       // console.log('res', res);
